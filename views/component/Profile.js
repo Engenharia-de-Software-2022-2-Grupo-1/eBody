e664bf3fa19d8daea4a7ef3d01d89207a5f50c59
alfredo.andrade <alfredo.andrade@virtus.ufcg.edu.br>
2023-05-17 14:57:25 -0300

    estilização dados Aluno

--- views/component/Profile.js
+++ views/component/Profile.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { Text, View, Button } from "react-native";
+import { profileCss } from '../../assets/css/ProfileCss'
 
 export default function Profile(props) {
     return(
-        <View>
-            <Text>Esse é o Profile de id {props.route.params.id}</Text>
+        <View style={[profileCss.container]}>
+            <Text style={profileCss.title}>Informações pessoais do aluno</Text>
+            <Text style={profileCss.title}>Nome <Text style={profileCss.data}>{props.route.params.name}</Text></Text>
+            <Text style={profileCss.title}>Contato <Text style={profileCss.data}>{props.route.params.phoneNumber}</Text></Text>
+            <Text style={profileCss.title}>Cidade <Text style={profileCss.data}>{props.route.params.city}</Text></Text>
+            <Text style={profileCss.title}>Bairo <Text style={profileCss.data}>{props.route.params.neighborhood}</Text></Text>
+            <Text style={profileCss.title}>Rua <Text style={profileCss.data}>{props.route.params.street}</Text></Text>
+            <Text style={profileCss.title}>Data de nascimento <Text style={profileCss.data}>{props.route.params.birthday}</Text></Text>
+
+
             <Button
             title="Editar"
             onPress={() => props.navigation.navigate('EditProfile')}
