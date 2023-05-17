e14104e49ab4c71919a11c8f7c4168949052aa60
thaisrocine_ <69332861+thaisrocine@users.noreply.github.com>
2023-05-16 21:12:17 -0300

    commit começo do pendente pagamento

--- views/component/PendingPayments.js
+++ views/component/PendingPayments.js
@@ -1,12 +1,92 @@
-import React from "react";
-import { Text, View, TouchableOpacity, Image } from "react-native";
-import { css } from "../../assets/css/Css";
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Box, Heading, FlatList, HStack, Avatar, VStack, Spacer, Center } from 'native-base';
+import { css } from '../../assets/css/Css';
 
 export default function PendingPayments(props) {
+  const data = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      fullName: 'Aafreen Khan',
+      timeStamp: '12:47 PM',
+      recentText: 'Good Day!',
+      avatarUrl: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      fullName: 'Sujitha Mathur',
+      timeStamp: '11:11 PM',
+      recentText: 'Cheer up, there!',
+      avatarUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEaZqT3fHeNrPGcnjLLX1v_W4mvBlgpwxnA&usqp=CAU',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      fullName: 'Anci Barroco',
+      timeStamp: '6:22 PM',
+      recentText: 'Good Day!',
+      avatarUrl: 'https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg',
+    },
+    {
+      id: '68694a0f-3da1-431f-bd56-142371e29d72',
+      fullName: 'Aniket Kumar',
+      timeStamp: '8:56 PM',
+      recentText: 'All the best',
+      avatarUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU',
+    },
+    {
+      id: '28694a0f-3da1-471f-bd96-142456e29d72',
+      fullName: 'Kiara',
+      timeStamp: '12:47 PM',
+      recentText: 'I will call today.',
+      avatarUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU',
+    },
+  ];
 
-    return(
-        <View style={css.container2}>
-           <Text>Pendentes</Text>
-        </View>
-    );
+  return (
+   
+ 
+    <View >
+       
+    
+
+    <Box>
+    <Heading fontSize="xl" p="15" pb="25" color="#888686" my={0} textAlign="center">
+  PENDENTES
+</Heading>
+
+  <FlatList
+  data={data}
+  renderItem={({ item }) => (
+   
+    <Box
+    bg="white"
+      borderRadius="sm"
+      shadow={1}
+      p="5"
+      my="1"
+      width="100%"
+    >
+      <HStack space={[2, 3]} justifyContent="space-between">
+        <VStack>
+
+        <Center>
+  <Text fontSize="xl" _dark={{ color: '#FFFFFF' }} color="#FFFFFF" bold>
+    {item.fullName}
+  </Text>
+</Center>
+
+        
+        </VStack>
+        <Spacer />
+      </HStack>
+    </Box>
+    
+  )}
+  keyExtractor={(item) => item.id}
+/>
+
+
+</Box>
+</View>
+);
 }
