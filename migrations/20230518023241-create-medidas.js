0ecb4e76280367266ca10f9b3d155bf71702f65a
Paulo Victor <paulo.cavalcanti@ccc.ufcg.edu.br>
2023-05-17 23:32:57 -0300

    criando model medidas

---
+++ migrations/20230518023241-create-medidas.js
@@ -0,0 +1,61 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Medidas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      data: {
+        type: Sequelize.DATE
+      },
+      peso: {
+        type: Sequelize.FLOAT
+      },
+      peito: {
+        type: Sequelize.FLOAT
+      },
+      ombro: {
+        type: Sequelize.FLOAT
+      },
+      cintura: {
+        type: Sequelize.FLOAT
+      },
+      quadril: {
+        type: Sequelize.FLOAT
+      },
+      bracoDireito: {
+        type: Sequelize.FLOAT
+      },
+      bracoEsquerdo: {
+        type: Sequelize.FLOAT
+      },
+      coxaDireita: {
+        type: Sequelize.FLOAT
+      },
+      coxaEsquerda: {
+        type: Sequelize.FLOAT
+      },
+      panturrilhaDireita: {
+        type: Sequelize.FLOAT
+      },
+      panturrilhaEsquerda: {
+        type: Sequelize.FLOAT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Medidas');
+  }
+};
