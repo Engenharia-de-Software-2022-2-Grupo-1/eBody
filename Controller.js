eb25e15a410fbdb02c5cc5cb63852b08a3479acf
Paulo Victor <paulo.cavalcanti@ccc.ufcg.edu.br>
2023-05-18 00:05:06 -0300

    cadastro de aluno

--- Controller.js
+++ Controller.js
@@ -1,10 +1,36 @@
 const express=require('express');
 const cors=require('cors');
 const bodyParser=require('body-parser');
+const models=require('./models');
+
 
 const app=express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+let aluno=models.Aluno;
+let mediddas=models.Medidas;
+
+
+app.post('/aluno',async (req,res)=>{
+    const { nome, dataNascimento, telefone, cidade, bairro, rua, adimplente } = req.body;
+    try {
+        const novoAluno=await aluno.create({
+            nome,
+            dataNascimento,
+            telefone,
+            cidade,
+            bairro,
+            rua,
+            adimplente
+        });
+        res.json(novoAluno);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'Erro ao criar aluno'});
+    }
+});
+
 
 let port=process.env.PORT || 3000;
 app.listen(port,(req,res)=>{
