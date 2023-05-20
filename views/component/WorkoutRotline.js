96d6ab4199288b8274bd51aca5b7c18cc5406180
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2023-05-19 22:31:50 -0300

    Update WorkoutRotline.js

--- views/component/WorkoutRotline.js
+++ views/component/WorkoutRotline.js
@@ -1,3 +1,4 @@
+import { Center } from "native-base";
 import React, {Component, useState} from "react";
 import { Text, View, TouchableOpacity, Image, Button, StyleSheet, FlatList } from "react-native";
 
@@ -34,22 +35,36 @@ export default function WorkoutRotline (props) {
                     repeticoes: 15,
                     series: 4,
                 },
-
-                
         ]};
 
         const renderItem = ({ item }) => {
             return (
-              <View style={styles.container}>
-                <View style={styles.containerExercicios}>
-                    <Text style={{ fontWeight: 'bold' }}>Nome: {item.nome}</Text>
-                    <Text>Repetições: {item.repeticoes}</Text>
-                    {item.series &&(
-                    <Text>Series: {item.series}</Text>
-                    )}
+              <View style={[styles.container,{paddingLeft: 20, paddingRight: 20}]}>
+                <View style={[styles.containerExercicios, {borderWidth: 1, borderColor: '#455d3b'}]}>
+                  <View style={{justifyContent: 'space-evenly'}}>
+                    <View style={{alignSelf: 'center',}}>
+                      <Text style={{ fontWeight: 'bold', fontSize:19 }}>{item.nome}</Text>
+                    </View>
+                    <View style={{flexDirection: 'row', justifyContent: 'space-around' }}>
+                        
+                        {item.series &&(
+                          <View>
+                            <Text>{item.series}</Text>
+                            <Text>Séries</Text>
+                            </View>
+                        )}
+                        
+                        <View >
+                          <Text>{item.repeticoes}</Text>
+                          <Text>Repetições</Text>
+                        </View>
+                        
+                      
+                    </View>
+                  </View>
                 </View>
                 {item.treino && item.treino.length > 0 && (
-                  <View style={{ paddingLeft: 20 }}>
+                  <View style={{ paddingLeft: 20, paddingRight: 20, }}>
                     {renderTreino(item.treino)}
                   </View>
                 )}
@@ -59,14 +74,28 @@ export default function WorkoutRotline (props) {
         
           const renderTreino = (treino) => {
             return treino.map((treinoItem, index) => (
-              <View key={index} style={styles.container}>
-                <View style={styles.containerExerciciosFilho}>
-                    <Text>Nome: {treinoItem.nome}</Text>
-                    <Text>Repetições: {treinoItem.repeticoes}</Text>
-                    {treinoItem.series &&(
-                        <Text>Series: {treinoItem.series}</Text>
-                    )}
-                </View>
+              <View style={[styles.containerExerciciosFilho, {borderWidth: 1,borderColor: '#455d3b',}]}>
+                  <View style={{justifyContent: 'space-evenly', }}>
+                    <View style={{alignSelf: 'center',}}>
+                      <Text style={{ fontWeight: 'bold', fontSize:19 }}>{treinoItem.nome}</Text>
+                    </View>
+                    <View style={{flexDirection: 'row', justifyContent: 'space-around' }}>
+                        
+                          {treinoItem.series &&(
+                            <View>
+                              <Text>{treinoItem.series}</Text>
+                              <Text>Séries</Text>
+                              </View>
+                          )}
+                        <View >
+                          <Text>{treinoItem.repeticoes}</Text>
+                          <Text>Repetições</Text>
+                        </View>
+                        
+                      
+                    </View>
+                  </View>
+                
                 
                 {treinoItem.treino && treinoItem.treino.length > 0 && (
                   <View >
@@ -76,34 +105,38 @@ export default function WorkoutRotline (props) {
               </View>
             ));
           };
-    
+     
         return(
                 <FlatList 
                     data={state.treino}
                     renderItem={renderItem}
                 />
 
-        );
+        ); 
 }
 
 const styles = StyleSheet.create({
     container:{
-        flex: 1 
+        flex: 1 ,
+    },
+    coluna:{
+      flexDirection: "row"
     },
     containerExercicios:{
-        backgroundColor: '#70964B',
+        backgroundColor: '#9bc063',
         heigh: 80,
-        margin: 5,
+        marginBottom: 0,
+        marginTop: 5,
         borderRadius: 10,
         paddingLeft: 10
     },
     containerExerciciosFilho:{
         backgroundColor: '#70964B',
         heigh: 80,
-        margin: 5,
-        borderRadius: 10,
-        
-        paddingLeft: 10
+        margin: 0,
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
+      
     },
     textTreino:{
         color:'white'
