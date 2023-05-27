8103212b73115b533a7313c7da9b1bfbe1e59643
Paulo Victor <paulo.cavalcanti@ccc.ufcg.edu.br>
2023-05-27 16:03:59 -0300

    criando o model treino

---
+++ models/treino.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Treino extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+        Treino.belongsTo(models.Aluno);
+        Treino.hasMany(models.Exercicio);
+    }
+  }
+  Treino.init({
+    nome: DataTypes.STRING,
+    alunoId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Treino',
+  });
+  return Treino;
+};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const alunoController = require('./controller/AlunoController');
 const medidasController = require('./controller/MedidasController');
+const treinoController = require('./controller/TreinoController');
 
 const app = express();
 const port = 3000;
@@ -14,6 +15,7 @@ app.use(bodyParser.json());
 
 app.use('/', alunoController);
 app.use('/', medidasController);
+app.use('/', treinoController);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
