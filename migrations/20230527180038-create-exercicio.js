a7076369c82cf712d85372756c99d028d8696432
Paulo Victor <paulo.cavalcanti@ccc.ufcg.edu.br>
2023-05-27 16:04:30 -0300

    criando model exercicio

---
+++ migrations/20230527180038-create-exercicio.js
@@ -0,0 +1,51 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Exercicios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING
+      },
+      repeticoes: {
+        type: Sequelize.INTEGER
+      },
+      series: {
+        type: Sequelize.INTEGER
+      },
+      conjugadoId: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: 'exercicios',
+            key: 'id'
+        },
+        onDelete: 'cascade'
+      },
+      treinoId: {
+        type: Sequelize.INTEGER,
+        references: {
+            model: 'treinos',
+            key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Exercicios');
+  }
+};
