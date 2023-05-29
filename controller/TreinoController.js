eecc4f60fff1587f6fb15478159d9059ad9e27c4
Paulo Victor <paulo.cavalcanti@ccc.ufcg.edu.br>
2023-05-29 20:48:56 -0300

    atualizar treino

--- controller/TreinoController.js
+++ controller/TreinoController.js
@@ -30,7 +30,7 @@ app.post('/aluno/:id/treino', async (req, res) => {
     }
 });
 
-
+// Adiciona exercicio no treino
 async function criarExercicios(exercicios, idTreino) {
     for (const exercicio of exercicios) {
         const { nome, repeticoes, series, conjugado } = exercicio;
@@ -54,6 +54,50 @@ async function criarExercicios(exercicios, idTreino) {
         }
     }
 }
+
+
+
+app.put('/aluno/:id/treino/:idTreino', async (req, res) => {
+    const { id, idTreino } = req.params;
+    const { nome, exercicios } = req.body;
+
+
+    try {
+        const aluno = await Aluno.findByPk(id);
+
+        if (aluno) {
+            const treino = await Treino.findOne({
+                where: {id: idTreino, alunoId: id}
+            });
+
+            if (treino) {
+                await treino.update({
+                    nome
+                });
+
+                await Exercicio.destroy({
+                    where: { treinoId: treino.id}
+                });
+
+                await criarExercicios(exercicios, treino.id);
+
+                res.json({ message: 'Treino atualizado com sucesso' });
+            } else {
+                res.status(404).json({ error: 'Treino não encontrado' });
+            }
+        } else {
+            res.json({ message: 'Aluno não encontrado '});
+
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro ao atualizar treino' });
+    }
+});
+
+
+
+  
 
 
 module.exports = app;
