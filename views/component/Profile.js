b639114e06ca769598de279236a4e81db84eb919
thaisrocine_ <69332861+thaisrocine@users.noreply.github.com>
2023-05-29 23:45:27 -0300

    menu drop down e edição do botão de editar

--- views/component/Profile.js
+++ views/component/Profile.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
-import { Text, View, Button, ScrollView } from "react-native";
-import { Select } from "native-base";
+import { Text, View, ScrollView } from "react-native";
+import { Select, Button } from "native-base";
 import { profileCss } from '../../assets/css/ProfileCss'
 
 export default function Profile(props) {
     let avaliacoes = [
-        { studentId: 1, weight: 81.2, height: 181, chest:60, shoulder:80, waist: 60, hip:70, date: new Date("2022-03-04T00:00:00.000Z"), rightArm: 30, leftArm: 30, rightThigh: 60, leftThigh: 60, rightCalf: 30, leftCalf: 30 },
-        { studentId: 2, weight: 2, height: 2, chest: 2, shoulder: 2, waist: 2, hip: 2 , date: new Date("2022-02-03T00:00:00.000Z"), rightArm: 2, leftArm: 2, rightThigh: 2, leftThigh: 2, rightCalf: 2, leftCalf: 2 },
+        { studentId: 1, weight: 81.2, height: 181, chest: 60, shoulder: 80, waist: 60, hip: 70, date: new Date("2022-03-04T00:00:00.000Z"), rightArm: 30, leftArm: 30, rightThigh: 60, leftThigh: 60, rightCalf: 30, leftCalf: 30 },
+        { studentId: 2, weight: 2, height: 2, chest: 2, shoulder: 2, waist: 2, hip: 2, date: new Date("2022-02-03T00:00:00.000Z"), rightArm: 2, leftArm: 2, rightThigh: 2, leftThigh: 2, rightCalf: 2, leftCalf: 2 },
         { studentId: 3, weight: 1, height: 1, chest: 1, shoulder: 1, waist: 1, hip: 1, date: new Date("2022-01-02T00:00:00.000Z"), rightArm: 1, leftArm: 1, rightThigh: 1, leftThigh: 1, rightCalf: 1, leftCalf: 1 }
     ];
 
     const [avaliacao, setAvaliacao] = useState(avaliacoes[0]);
 
-    return(
+    return (
         <ScrollView>
             <View style={[profileCss.container]}>
                 <Text style={profileCss.title}>Informações pessoais do aluno</Text>
@@ -21,10 +21,10 @@ export default function Profile(props) {
                     <Text style={profileCss.title}>Nome </Text>
                     <Text style={profileCss.data}>{props.route.params.name}</Text>
 
-                    <Text style={profileCss.title}>Contato</Text> 
+                    <Text style={profileCss.title}>Contato</Text>
                     <Text style={profileCss.data}>{props.route.params.phoneNumber}</Text>
 
-                    <Text style={profileCss.title}>Cidade</Text> 
+                    <Text style={profileCss.title}>Cidade</Text>
                     <Text style={profileCss.data}>{props.route.params.city}</Text>
 
                     <Text style={profileCss.title}>Bairo</Text>
@@ -37,22 +37,22 @@ export default function Profile(props) {
                     <Text style={profileCss.data}>{props.route.params.birthday}</Text>
                 </View>
             </View>
-            
-            <View style={[profileCss.container, {marginBottom:15}]}>
+
+            <View style={[profileCss.container, { marginBottom: 15 }]}>
                 <Text style={profileCss.title}>Informações fisicas do aluno</Text>
             </View>
-            
-            {!avaliacoes?(
+
+            {!avaliacoes ? (
                 <View style={[profileCss.container]}>
                     <Text>Sem Avaliações encontradas</Text>
                 </View>
-            ):(
+            ) : (
                 <View style={[profileCss.container]}>
 
                     <Select minWidth="250" placeholder="Escolha a avaliação"
-                    onValueChange={itemValue => setAvaliacao(itemValue)} style={{marginTop: 10, marginBottom: 10}}>
-                        {avaliacoes.map((av)=>{
-                            return <Select.Item label={av.date.toLocaleDateString('pt-PT')} value={av} key={av.weight}/>
+                        onValueChange={itemValue => setAvaliacao(itemValue)} style={{ marginTop: 10, marginBottom: 10 }}>
+                        {avaliacoes.map((av) => {
+                            return <Select.Item label={av.date.toLocaleDateString('pt-PT')} value={av} key={av.weight} />
                         })}
                     </Select>
 
@@ -68,7 +68,7 @@ export default function Profile(props) {
 
                         <Text style={profileCss.title}>Ombro</Text>
                         <Text style={profileCss.data}>{avaliacao.shoulder}</Text>
-                        
+
                         <Text style={profileCss.title}>Cintura</Text>
                         <Text style={profileCss.data}>{avaliacao.waist}</Text>
 
@@ -77,19 +77,19 @@ export default function Profile(props) {
 
                         <Text style={profileCss.title}>Biceps Direito</Text>
                         <Text style={profileCss.data}>{avaliacao.rightArm}</Text>
-                        
+
                         <Text style={profileCss.title}>Biceps Esquerdo</Text>
                         <Text style={profileCss.data}>{avaliacao.leftArm}</Text>
 
                         <Text style={profileCss.title}>Coxa Direita</Text>
                         <Text style={profileCss.data}>{avaliacao.rightThigh}</Text>
-                        
+
                         <Text style={profileCss.title}>Coxa Esquerda</Text>
                         <Text style={profileCss.data}>{avaliacao.leftThigh}</Text>
 
                         <Text style={profileCss.title}>Panturrilha Direita</Text>
                         <Text style={profileCss.data}>{avaliacao.rightCalf}</Text>
-                        
+
                         <Text style={profileCss.title}>Panturrilha Esquerda</Text>
                         <Text style={profileCss.data}>{avaliacao.leftCalf}</Text>
                     </View>
@@ -98,22 +98,22 @@ export default function Profile(props) {
 
             <View><Text></Text></View>
 
+
             <Button
-            title="Editar"
-            onPress={() => props.navigation.navigate('EditProfile', props.route.params)}
-            />
-            <Button
-            title="Adicionar medidas"
-            onPress={() => props.navigation.navigate('NewEvaluation')}
-            />
-            <Button
-            title="Visualizar treinos"
-            onPress={() => props.navigation.navigate('MuscularGroups')}
-            />
-            <Button
-            title="Gráficos de medidas"
-            onPress={() => props.navigation.navigate('Measurement')}
-            />
+                style={{
+                    borderRadius: 10,
+                    height: 40,
+                    justifyContent: "center",
+                    backgroundColor: "#455D3B",
+                }}
+                onPress={() => props.navigation.navigate('EditProfile', props.route.params)}>
+                <Text style={{ color: "white", fontWeight: "bold" }}>EDITAR</Text>
+            </Button>
+
+
+
+
+
         </ScrollView>
     );
 }
--- views/stack/StudentsStackScreen.js
+++ views/stack/StudentsStackScreen.js
@@ -1,34 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import {Students, Profile, NewProfileScreen, EditStudentScreen, NewEvaluation, MuscularGroups, WorkoutRotline, MeasurementScreen, Training} from '../component/Index'
+import { Students, Profile, NewProfileScreen, EditStudentScreen, NewEvaluation, MuscularGroups, WorkoutRotline, MeasurementScreen, Training } from '../component/Index'
+import { Menu, Pressable, HamburgerIcon, Box, Divider, Text, Button} from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
 
 function StudentsStackScreen() {
 
-    const StudentsStack = createNativeStackNavigator();
-    
-    return(
-        <StudentsStack.Navigator>
-            <StudentsStack.Screen 
-                name="StudentsScreen" 
-                component={Students}
-                options={{
-                    title:"Estudantes",
-                    headerStyle:{backgroundColor: '#455d3b'},
-                    headerTintColor:'white',
-                    headerTitleStyle:{fontWeight:'bold'},
-                    headerTitleAlign: 'center'
-                }}
-            />
-            <StudentsStack.Screen 
-                name="Profile" 
-                component={Profile}
-                options={({ route }) => ({ 
-                    title: route.params.name,
-                    headerStyle:{backgroundColor: '#455d3b'},
-                    headerTintColor:'white',
-                    headerTitleStyle:{fontWeight:'bold'},
-                    headerTitleAlign: 'center'
-                    })}/>
+  const StudentsStack = createNativeStackNavigator();
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleMenuOpen = () => {
+    setIsMenuOpen(true);
+  };
+
+  const handleMenuClose = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <StudentsStack.Navigator>
+      <StudentsStack.Screen 
+        name="StudentsScreen" 
+        component={Students}
+        options={{
+          title: "Estudantes",
+          headerStyle: { backgroundColor: '#455d3b' },
+          headerTintColor: 'white',
+          headerTitleStyle: { fontWeight: 'bold' },
+          headerTitleAlign: 'center'
+        }}
+      />
+
+      <StudentsStack.Screen
+        name="Profile"
+        component={Profile}
+        options={({ route, navigation }) => ({
+          title: route.params.name,
+          headerStyle: { backgroundColor: '#455d3b' },
+          headerTintColor: 'white',
+          headerTitleStyle: { fontWeight: 'bold' },
+          headerTitleAlign: 'center',
+          headerRight: () => (
+            <Box flexDirection="row" alignItems="center">
+              <Menu
+                isOpen={isMenuOpen}
+                onClose={handleMenuClose}
+                onOpen={handleMenuOpen}
+                trigger={(triggerProps) => (
+                  <Pressable {...triggerProps}>
+                    <HamburgerIcon />
+                  </Pressable>
+                )}
+                placement="bottom end"
+              >
+             
+                <Menu.Item
+                  onPress={() => {
+                    setSelectedOption('Opção 2');
+                    handleMenuClose();
+                    navigation.navigate('NewEvaluation'); // Redireciona para a tela 'NewEvaluation'
+                  }}
+                >
+                  ADICIONAR MEDIDAS
+                </Menu.Item>
+                <Menu.Item
+                  onPress={() => {
+                    setSelectedOption('Opção 3');
+                    handleMenuClose();
+                    navigation.navigate('MuscularGroups'); // Redireciona para a tela 'MuscularGroups'
+                  }}
+                >
+                  VISUALIZAR TREINOS
+                </Menu.Item>
+                <Menu.Item
+                  onPress={() => {
+                    setSelectedOption('Opção 4');
+                    handleMenuClose();
+                    navigation.navigate('Measurement'); // Redireciona para a tela 'Measurement'
+                  }}
+                >
+                  GRÁFICOS DE MEDIDAS
+                </Menu.Item>
+              </Menu>
+            </Box>
+          ),
+        })}
+      />
             <StudentsStack.Screen
                 name="NewProfile"
                 component={NewProfileScreen}
@@ -109,6 +168,7 @@ function StudentsStackScreen() {
             />
             
         </StudentsStack.Navigator>
+  
     );
 
 }
