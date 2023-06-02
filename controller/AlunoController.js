a5d521881ee79e54da02633847bd1ec5a550f1aa
Paulo Victor <paulo.cavalcanti@ccc.ufcg.edu.br>
2023-06-01 21:42:12 -0300

    refatorando estilo do código com ESlint

--- controller/AlunoController.js
+++ controller/AlunoController.js
@@ -9,204 +9,204 @@ const app = express.Router();
 let Aluno = models.Aluno;
 
 app.post('/aluno', async (req, res) => {
-    const { nome, dataNascimento, telefone, cidade, bairro, adimplente,
-        nomeContato1, numeroContato1, grauContato1, nomeContato2, numeroContato2, grauContato2 } = req.body;
+	const { nome, dataNascimento, telefone, cidade, bairro, adimplente,
+		nomeContato1, numeroContato1, grauContato1, nomeContato2, numeroContato2, grauContato2 } = req.body;
     
-    const dataPagamento = new Date();
-    try {
-        const aluno = await Aluno.findOne({ where: { nome, dataNascimento } });
-        if (aluno) {
-            res.status(409).json({ message: 'Esse aluno já está cadastrado!' });
-        } else {
-            await Aluno.create({
-                nome,
-                dataNascimento,
-                telefone,
-                cidade,
-                bairro,
-                dataPagamento,
-                adimplente,
-                nomeContato1,
-                numeroContato1,
-                grauContato1,
-                nomeContato2,
-                numeroContato2,
-                grauContato2
-            });
-            res.status(201).json({ message: 'Aluno cadastrado com sucesso' });
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Erro ao criar aluno' });
-    }
+	const dataPagamento = new Date();
+	try {
+		const aluno = await Aluno.findOne({ where: { nome, dataNascimento } });
+		if (aluno) {
+			res.status(409).json({ message: 'Esse aluno já está cadastrado!' });
+		} else {
+			await Aluno.create({
+				nome,
+				dataNascimento,
+				telefone,
+				cidade,
+				bairro,
+				dataPagamento,
+				adimplente,
+				nomeContato1,
+				numeroContato1,
+				grauContato1,
+				nomeContato2,
+				numeroContato2,
+				grauContato2
+			});
+			res.status(201).json({ message: 'Aluno cadastrado com sucesso' });
+		}
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Erro ao criar aluno' });
+	}
 });
 
 app.get('/aluno', async (req, res) => {
-    try {
-        const alunos = await Aluno.findAll();
-        res.status(200).json(alunos);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Erro ao listar alunos' });
-    }
+	try {
+		const alunos = await Aluno.findAll();
+		res.status(200).json(alunos);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Erro ao listar alunos' });
+	}
 });
 
 app.get('/aluno/nome/:nome', async (req, res) => {
-    const { nome } = req.params;
-    try {
-        const alunos = await Aluno.findAll({
-            where: {
-                nome: {
-                    [Op.like]: `%${nome}%`,
-                },
-            },
-        });
-        if (alunos.length > 0) {
-            res.status(200).json(alunos);
-        } else {
-            res.status(200).json({ message: 'Nenhum aluno encontrado' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao buscar os alunos pelo nome.' });
-    }
+	const { nome } = req.params;
+	try {
+		const alunos = await Aluno.findAll({
+			where: {
+				nome: {
+					[Op.like]: `%${nome}%`,
+				},
+			},
+		});
+		if (alunos.length > 0) {
+			res.status(200).json(alunos);
+		} else {
+			res.status(200).json({ message: 'Nenhum aluno encontrado' });
+		}
+	} catch (error) {
+		res.status(500).json({ error: 'Erro ao buscar os alunos pelo nome.' });
+	}
 });
 
 app.get('/aluno/:id', async (req, res) => {
-    const { id } = req.params;
-    try {
-        const aluno = await Aluno.findByPk(id);
+	const { id } = req.params;
+	try {
+		const aluno = await Aluno.findByPk(id);
 
-        if (aluno) {
-            res.status(200).json(aluno);
-        } else {
-            res.status(404).json({ error: 'Aluno não encontrado' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao obter o aluno pelo ID.' });
-    }
+		if (aluno) {
+			res.status(200).json(aluno);
+		} else {
+			res.status(404).json({ error: 'Aluno não encontrado' });
+		}
+	} catch (error) {
+		res.status(500).json({ error: 'Erro ao obter o aluno pelo ID.' });
+	}
 });
 
 app.get('/aniversariante/', async (req, res) => {
-    try {
-        const mesAtual = new Date().getMonth() + 1;
-        const aniversariantes = await Aluno.findAll({
-            attributes: ['nome', 'dataNascimento'],
-            where: sequelize.where(
-                sequelize.fn('strftime', '%m', sequelize.col('dataNascimento')),
-                mesAtual.toString().padStart(2, '0')
-            )
-        });
-        res.status(200).json(aniversariantes);
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao obter os aniversariantes do mês atual.' });
-    }
+	try {
+		const mesAtual = new Date().getMonth() + 1;
+		const aniversariantes = await Aluno.findAll({
+			attributes: ['id', 'nome', 'dataNascimento'],
+			where: sequelize.where(
+				sequelize.fn('strftime', '%m', sequelize.col('dataNascimento')),
+				mesAtual.toString().padStart(2, '0')
+			)
+		});
+		res.status(200).json(aniversariantes);
+	} catch (error) {
+		res.status(500).json({ error: 'Erro ao obter os aniversariantes do mês atual.' });
+	}
 });
 
 app.get('/inadimplente/', async (req, res) => {
-    try {
+	try {
 
-        await verificarInadimplencia();
+		await verificarInadimplencia();
 
-        const inadimplentes = await Aluno.findAll({
-            attributes: ['nome', 'adimplente'],
-            where: {
-                adimplente: false
-            }
-        });
-        res.status(200).json(inadimplentes);
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao obter os inadimplente do mês atual.' });
-    }
+		const inadimplentes = await Aluno.findAll({
+			attributes: ['id', 'nome', 'adimplente'],
+			where: {
+				adimplente: false
+			}
+		});
+		res.status(200).json(inadimplentes);
+	} catch (error) {
+		res.status(500).json({ error: 'Erro ao obter os inadimplente do mês atual.' });
+	}
 });
 
 app.put('/aluno/:id', async (req, res) => {
-    const { id } = req.params;
-    const { nome, dataNascimento, telefone, cidade, bairro, adimplente,
-        nomeContato1, numeroContato1, grauContato1, nomeContato2, numeroContato2, grauContato2 } = req.body;
+	const { id } = req.params;
+	const { nome, dataNascimento, telefone, cidade, bairro, adimplente,
+		nomeContato1, numeroContato1, grauContato1, nomeContato2, numeroContato2, grauContato2 } = req.body;
 
-    try {
-        const alunoAtualizado = await Aluno.findByPk(id);
+	try {
+		const alunoAtualizado = await Aluno.findByPk(id);
 
-        if (alunoAtualizado) {
-            await alunoAtualizado.update({
-                nome,
-                dataNascimento,
-                telefone,
-                cidade,
-                bairro,
-                adimplente,
-                nomeContato1,
-                numeroContato1,
-                grauContato1,
-                nomeContato2,
-                numeroContato2,
-                grauContato2
-            });
-            res.status(200).json({ message: 'Aluno atualizado com sucesso' });
-        } else {
-            res.status(404).json({ error: 'Aluno não encontrado' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao atualizar aluno' });
-    }
+		if (alunoAtualizado) {
+			await alunoAtualizado.update({
+				nome,
+				dataNascimento,
+				telefone,
+				cidade,
+				bairro,
+				adimplente,
+				nomeContato1,
+				numeroContato1,
+				grauContato1,
+				nomeContato2,
+				numeroContato2,
+				grauContato2
+			});
+			res.status(200).json({ message: 'Aluno atualizado com sucesso' });
+		} else {
+			res.status(404).json({ error: 'Aluno não encontrado' });
+		}
+	} catch (error) {
+		res.status(500).json({ error: 'Erro ao atualizar aluno' });
+	}
 });
 
 app.put('/inadimplente/:id', async (req, res) => {
-    const { id } = req.params;
+	const { id } = req.params;
 
-    try {
-        const aluno = await Aluno.findByPk(id);
-        if (aluno) {
-            aluno.adimplente = true;
+	try {
+		const aluno = await Aluno.findByPk(id);
+		if (aluno) {
+			aluno.adimplente = true;
 
-            const novaDataPagamento = moment(aluno.dataPagamento).add(1, 'months').toDate();
-            aluno.dataPagamento = novaDataPagamento;
+			const novaDataPagamento = moment(aluno.dataPagamento).add(1, 'months').toDate();
+			aluno.dataPagamento = novaDataPagamento;
 
-            await aluno.save();
-            res.status(200).json({ message: 'Aluno atualizado com sucesso' });
-        } else {
-            res.status(404).json({ error: 'Aluno não encontrado' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao atualizar aluno' });
-    }
+			await aluno.save();
+			res.status(200).json({ message: 'Aluno atualizado com sucesso' });
+		} else {
+			res.status(404).json({ error: 'Aluno não encontrado' });
+		}
+	} catch (error) {
+		res.status(500).json({ error: 'Erro ao atualizar aluno' });
+	}
 });
 
 app.delete('/aluno/:id', async (req, res) => {
-    const { id } = req.params;
-    try {
-        const aluno = await Aluno.findByPk(id);
-        if (aluno) {
-            await Aluno.destroy({ where: { id } });
-            res.status(200).json({ message: 'Aluno excluido com sucesso' });
-        } else {
-            res.status(404).json({ error: 'Aluno não encontrado' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao excluir aluno' });
-    }
+	const { id } = req.params;
+	try {
+		const aluno = await Aluno.findByPk(id);
+		if (aluno) {
+			await Aluno.destroy({ where: { id } });
+			res.status(200).json({ message: 'Aluno excluido com sucesso' });
+		} else {
+			res.status(404).json({ error: 'Aluno não encontrado' });
+		}
+	} catch (error) {
+		res.status(500).json({ error: 'Erro ao excluir aluno' });
+	}
 });
 
 
 async function verificarInadimplencia() {
-    try {
-        const alunos = await Aluno.findAll();
+	try {
+		const alunos = await Aluno.findAll();
 
-        for (const aluno of alunos) {
-            const dataPagamento = moment(aluno.dataPagamento);
-            const dataAtual = moment();
-            const diffDias = dataAtual.diff(dataPagamento, 'days');
+		for (const aluno of alunos) {
+			const dataPagamento = moment(aluno.dataPagamento);
+			const dataAtual = moment();
+			const diffDias = dataAtual.diff(dataPagamento, 'days');
 
 
-            if (diffDias >= 30) {
-                aluno.adimplente = false;
-                await aluno.save();
-            }
-        }
-        console.log('Verificação de inadimplência concluída.');
-    } catch (error) {
-        console.error('Erro ao verificar inadimplência:', error);
-    }
+			if (diffDias >= 30) {
+				aluno.adimplente = false;
+				await aluno.save();
+			}
+		}
+		console.log('Verificação de inadimplência concluída.');
+	} catch (error) {
+		console.error('Erro ao verificar inadimplência:', error);
+	}
 }
 
 module.exports = app;
--- controller/TreinoController.js
+++ controller/TreinoController.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const models = require('../models');
-const sequelize = require('sequelize');
-
 
 const app = express.Router();
 let Aluno = models.Aluno;
@@ -10,137 +8,156 @@ let Exercicio = models.Exercicio;
 
 
 app.post('/aluno/:id/treino', async (req, res) => {
-    const { id } = req.params;
-    const { nome, exercicios } = req.body;
+	const { id } = req.params;
+	const { nome, exercicios } = req.body;
 
-    try {
-        const aluno = await Aluno.findByPk(id);
+	try {
+		const aluno = await Aluno.findByPk(id);
 
-        if (aluno) {
-            const treino = await Treino.create({
-                nome,
-                alunoId: aluno.id
-            });
+		if (aluno) {
+			const treino = await Treino.create({
+				nome,
+				alunoId: aluno.id
+			});
 
-            const exerciciosData = exercicios.map(exercicio => ({
-                nome: exercicio.nome,
-                repeticoes: exercicio.repeticoes,
-                series: exercicio.series,
-                treinoId: treino.id
-            }));
+			const exerciciosData = exercicios.map(exercicio => ({
+				nome: exercicio.nome,
+				repeticoes: exercicio.repeticoes,
+				series: exercicio.series,
+				treinoId: treino.id
+			}));
 
-            await Exercicio.bulkCreate(exerciciosData);
+			await Exercicio.bulkCreate(exerciciosData);
 
-            res.status(201).json({ message: 'Treino do aluno cadastrado com sucesso' });
-        } else {
-            res.status(404).json({ message: 'Aluno não encontrado' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao cadastrar treino' });
-    }
+			res.status(201).json({ message: 'Treino do aluno cadastrado com sucesso' });
+		} else {
+			res.status(404).json({ message: 'Aluno não encontrado' });
+		}
+	} catch (error) {
+		res.status(500).json({ error: 'Erro ao cadastrar treino' });
+	}
 });
 
 
 app.get('/aluno/:id/treino/:treinoId', async (req, res) => {
-    const { id, treinoId } = req.params;
+	const { id, treinoId } = req.params;
 
-    try {
-        const aluno = await Aluno.findByPk(id);
+	try {
+		const aluno = await Aluno.findByPk(id);
 
-        if (aluno) {
-            const treino = await Treino.findOne({
-                where: { id: treinoId },
-                include: Exercicio
-            });
+		if (aluno) {
+			const treino = await Treino.findOne({
+				where: { id: treinoId },
+				include: Exercicio
+			});
 
-            if (treino) {
-                res.status(200).json(treino);
-            } else {
-                res.status(404).json({ error: 'Treino não encontrado' });
-            }
+			if (treino) {
+				res.status(200).json(treino);
+			} else {
+				res.status(404).json({ error: 'Treino não encontrado' });
+			}
 
-        } else {
-            res.status(404).json({ message: 'Aluno não encontrado' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao obter treino' });
-    }
+		} else {
+			res.status(404).json({ message: 'Aluno não encontrado' });
+		}
+	} catch (error) {
+		res.status(500).json({ error: 'Erro ao obter treino' });
+	}
 });
 
+app.get('/aluno/:id/treino/', async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		const aluno = await Aluno.findByPk(id);
+
+		if (aluno) {
+			const treinos = await Treino.findAll({
+				where: { alunoId: id }
+			});
+			res.status(200).json(treinos);
+		} else {
+			res.status(404).json({ error: 'Aluno não encontrado' });
+		}
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Erro ao obter treinos do aluno' });
+	}
+});
 
 app.put('/aluno/:id/treino/:treinoId', async (req, res) => {
-    const { id, treinoId } = req.params;
-    const { nome, exercicios } = req.body;
+	const { id, treinoId } = req.params;
+	const { nome, exercicios } = req.body;
 
-    try {
-        const aluno = Aluno.findByPk(id);
+	try {
+		const aluno = Aluno.findByPk(id);
 
-        if (aluno) {
-            const treino = await Treino.findOne({
-                where: { id: treinoId }
-            });
-            if (treino) {
+		if (aluno) {
+			const treino = await Treino.findOne({
+				where: { id: treinoId }
+			});
+			if (treino) {
 
-                await treino.update({
-                    nome
-                });
+				await treino.update({
+					nome
+				});
 
-                await Exercicio.destroy({
-                    where: { treinoId: treino.id }
-                });
+				await Exercicio.destroy({
+					where: { treinoId: treino.id }
+				});
 
-                const exerciciosData = exercicios.map(exercicio => ({
-                    nome: exercicio.nome,
-                    repeticoes: exercicio.repeticoes,
-                    series: exercicio.series,
-                    treinoId: treino.id
-                }));
+				const exerciciosData = exercicios.map(exercicio => ({
+					nome: exercicio.nome,
+					repeticoes: exercicio.repeticoes,
+					series: exercicio.series,
+					treinoId: treino.id
+				}));
 
-                await Exercicio.bulkCreate(exerciciosData);
+				await Exercicio.bulkCreate(exerciciosData);
 
-                res.status(200).json({ message: 'Treino atualizado com sucesso' });
-            } else {
-                res.status(404).json({ error: 'Treino não encontrado' });
-            }
-        } else {
-            res.status(404).json({ message: 'Aluno não encontrado' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao atualizar treino' });
-    }
+				res.status(200).json({ message: 'Treino atualizado com sucesso' });
+			} else {
+				res.status(404).json({ error: 'Treino não encontrado' });
+			}
+		} else {
+			res.status(404).json({ message: 'Aluno não encontrado' });
+		}
+	} catch (error) {
+		res.status(500).json({ error: 'Erro ao atualizar treino' });
+	}
 });
 
 app.delete('/aluno/:id/treino/:idTreino', async (req, res) => {
-    const { id, idTreino } = req.params;
+	const { id, idTreino } = req.params;
 
-    try {
-        const aluno = await Aluno.findByPk(id);
+	try {
+		const aluno = await Aluno.findByPk(id);
 
-        if (aluno) {
-            const treino = await Treino.findOne({
-                where: { id: idTreino }
-            });
+		if (aluno) {
+			const treino = await Treino.findOne({
+				where: { id: idTreino }
+			});
 
-            if (treino) {
-                await Exercicio.destroy({
-                    where: { treinoId: idTreino }
-                });
+			if (treino) {
+				await Exercicio.destroy({
+					where: { treinoId: idTreino }
+				});
 
-                await Treino.destroy({
-                    where: { id: idTreino }
-                });
+				await Treino.destroy({
+					where: { id: idTreino }
+				});
 
-                res.status(200).json({ message: 'Treino excluído com sucesso' });
-            } else {
-                res.status(404).json({ error: 'Treino não encontrado' });
-            }
-        } else {
-            res.status(404).json({ message: 'Aluno não encontrado' });
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Erro ao excluir treino' });
-    }
+				res.status(200).json({ message: 'Treino excluído com sucesso' });
+			} else {
+				res.status(404).json({ error: 'Treino não encontrado' });
+			}
+		} else {
+			res.status(404).json({ message: 'Aluno não encontrado' });
+		}
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Erro ao excluir treino' });
+	}
 });
 
 module.exports = app;
